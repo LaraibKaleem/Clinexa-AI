8e532f021d937ea29de8370832dec16ce18138a4
LaraibKaleem <laraibkaleem15@gmail.com>
2026-05-08 20:53:49 +0500

    updated all files

--- phase2_mcp/fhir_mcp/fhir_server.py
+++ phase2_mcp/fhir_mcp/fhir_server.py
@@ -233,5 +233,6 @@ if __name__ == "__main__":
     # uvicorn.run(app, host="0.0.0.0", port=8001)
     import uvicorn
     import os
-    port = int(os.getenv("PORT", 8001))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    # port = int(os.getenv("PORT", 8001))
+    uvicorn.run(app, host="0.0.0.0", port=8001)
+    
